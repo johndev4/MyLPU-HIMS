5397f6e8cbc832167280e9da199357fe52c87b3e
johndev4 <raphmistica04@gmail.com>
2023-12-30 05:24:52 +0800

    0001: Dockerize initial commit

--- infrastructure/database/sql/mylpuhims-init-create-tables.sql
+++ infrastructure/database/sql/mylpuhims-init-create-tables.sql
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `mylpuhims_db`
 --
-CREATE SCHEMA mylpuhims_db;
+CREATE SCHEMA IF NOT EXISTS mylpuhims_db;
 
 -- --------------------------------------------------------
 
@@ -28,7 +28,7 @@ CREATE SCHEMA mylpuhims_db;
 -- Table structure for table `activity_logs`
 --
 
-CREATE TABLE `activity_logs` (
+CREATE TABLE IF NOT EXISTS mylpuhims_db.`activity_logs` (
   `log_id` int(11) NOT NULL,
   `enduser_id` varchar(45) NOT NULL,
   `enduser_type` varchar(45) NOT NULL,
@@ -48,7 +48,7 @@ CREATE TABLE `activity_logs` (
 -- Table structure for table `administrators`
 --
 
-CREATE TABLE `administrators` (
+CREATE TABLE IF NOT EXISTS mylpuhims_db.`administrators` (
   `admin_id` varchar(45) NOT NULL,
   `username` varchar(45) NOT NULL,
   `password` varchar(256) NOT NULL,
@@ -62,7 +62,7 @@ CREATE TABLE `administrators` (
 -- Table structure for table `batches`
 --
 
-CREATE TABLE `batches` (
+CREATE TABLE IF NOT EXISTS mylpuhims_db.`batches` (
   `batch_id` varchar(45) NOT NULL,
   `product_id` varchar(45) NOT NULL,
   `stock_in` int(11) NOT NULL,
@@ -78,7 +78,7 @@ CREATE TABLE `batches` (
 -- Table structure for table `consultations`
 --
 
-CREATE TABLE `consultations` (
+CREATE TABLE IF NOT EXISTS mylpuhims_db.`consultations` (
   `consultation_no` varchar(16) NOT NULL,
   `status` varchar(45) NOT NULL,
   `category` varchar(45) NOT NULL,
@@ -98,7 +98,7 @@ CREATE TABLE `consultations` (
 -- Table structure for table `equipments`
 --
 
-CREATE TABLE `equipments` (
+CREATE TABLE IF NOT EXISTS mylpuhims_db.`equipments` (
   `product_id` varchar(45) NOT NULL,
   `product_name` varchar(45) NOT NULL,
   `qty` int(11) NOT NULL,
@@ -112,7 +112,7 @@ CREATE TABLE `equipments` (
 -- Table structure for table `health_personnels`
 --
 
-CREATE TABLE `health_personnels` (
+CREATE TABLE IF NOT EXISTS mylpuhims_db.`health_personnels` (
   `id_no` varchar(45) NOT NULL,
   `first_name` varchar(45) NOT NULL,
   `middle_name` varchar(45) NOT NULL,
@@ -128,7 +128,7 @@ CREATE TABLE `health_personnels` (
 -- Table structure for table `health_personnels_account`
 --
 
-CREATE TABLE `health_personnels_account` (
+CREATE TABLE IF NOT EXISTS mylpuhims_db.`health_personnels_account` (
   `id_no` varchar(45) NOT NULL,
   `username` varchar(45) NOT NULL,
   `password` varchar(256) NOT NULL,
@@ -142,7 +142,7 @@ CREATE TABLE `health_personnels_account` (
 -- Table structure for table `health_personnels_notification`
 --
 
-CREATE TABLE `health_personnels_notification` (
+CREATE TABLE IF NOT EXISTS mylpuhims_db.`health_personnels_notification` (
   `notification_id` int(11) NOT NULL,
   `id_no` varchar(45) NOT NULL,
   `icon` varchar(100) NOT NULL,
@@ -159,7 +159,7 @@ CREATE TABLE `health_personnels_notification` (
 -- Table structure for table `health_records`
 --
 
-CREATE TABLE `health_records` (
+CREATE TABLE IF NOT EXISTS mylpuhims_db.`health_records` (
   `record_id` int(11) NOT NULL,
   `id_no` varchar(45) NOT NULL,
   `file_path` varchar(256) NOT NULL,
@@ -173,7 +173,7 @@ CREATE TABLE `health_records` (
 -- Table structure for table `lyceans`
 --
 
-CREATE TABLE `lyceans` (
+CREATE TABLE IF NOT EXISTS mylpuhims_db.`lyceans` (
   `id_no` varchar(45) NOT NULL,
   `first_name` varchar(45) NOT NULL,
   `middle_name` varchar(45) NOT NULL,
@@ -193,7 +193,7 @@ CREATE TABLE `lyceans` (
 -- Table structure for table `lyceans_account`
 --
 
-CREATE TABLE `lyceans_account` (
+CREATE TABLE IF NOT EXISTS mylpuhims_db.`lyceans_account` (
   `id_no` varchar(45) NOT NULL,
   `username` varchar(45) NOT NULL,
   `password` varchar(256) NOT NULL,
@@ -206,7 +206,7 @@ CREATE TABLE `lyceans_account` (
 -- Table structure for table `lyceans_notification`
 --
 
-CREATE TABLE `lyceans_notification` (
+CREATE TABLE IF NOT EXISTS mylpuhims_db.`lyceans_notification` (
   `notification_id` int(11) NOT NULL,
   `id_no` varchar(45) NOT NULL,
   `icon` varchar(100) NOT NULL,
@@ -223,7 +223,7 @@ CREATE TABLE `lyceans_notification` (
 -- Table structure for table `medical_files`
 --
 
-CREATE TABLE `medical_files` (
+CREATE TABLE IF NOT EXISTS mylpuhims_db.`medical_files` (
   `file_id` int(11) NOT NULL,
   `consultation_no` varchar(16) NOT NULL,
   `file_path` varchar(256) NOT NULL
@@ -235,7 +235,7 @@ CREATE TABLE `medical_files` (
 -- Table structure for table `medicines`
 --
 
-CREATE TABLE `medicines` (
+CREATE TABLE IF NOT EXISTS mylpuhims_db.`medicines` (
   `product_id` varchar(45) NOT NULL,
   `manufacturer` varchar(45) NOT NULL,
   `generic_name` varchar(100) NOT NULL,
@@ -251,26 +251,26 @@ CREATE TABLE `medicines` (
 --
 -- Indexes for table `activity_logs`
 --
-ALTER TABLE `activity_logs`
+ALTER TABLE mylpuhims_db.`activity_logs`
   ADD PRIMARY KEY (`log_id`);
 
 --
 -- Indexes for table `administrators`
 --
-ALTER TABLE `administrators`
+ALTER TABLE mylpuhims_db.`administrators`
   ADD PRIMARY KEY (`admin_id`);
 
 --
 -- Indexes for table `batches`
 --
-ALTER TABLE `batches`
+ALTER TABLE mylpuhims_db.`batches`
   ADD PRIMARY KEY (`batch_id`),
   ADD KEY `fk_batches_medicines1_idx` (`product_id`);
 
 --
 -- Indexes for table `consultations`
 --
-ALTER TABLE `consultations`
+ALTER TABLE mylpuhims_db.`consultations`
   ADD PRIMARY KEY (`consultation_no`),
   ADD KEY `fk_consultations_clinic_personnels1_idx` (`personnel_id_no`),
   ADD KEY `fk_consultations_lyceans1_idx` (`lycean_id_no`);
@@ -278,33 +278,33 @@ ALTER TABLE `consultations`
 --
 -- Indexes for table `equipments`
 --
-ALTER TABLE `equipments`
+ALTER TABLE mylpuhims_db.`equipments`
   ADD PRIMARY KEY (`product_id`);
 
 --
 -- Indexes for table `health_personnels`
 --
-ALTER TABLE `health_personnels`
+ALTER TABLE mylpuhims_db.`health_personnels`
   ADD PRIMARY KEY (`id_no`);
 
 --
 -- Indexes for table `health_personnels_account`
 --
-ALTER TABLE `health_personnels_account`
+ALTER TABLE mylpuhims_db.`health_personnels_account`
   ADD PRIMARY KEY (`id_no`),
   ADD KEY `fk_clinic_personnels_credentials_clinic_personnels_idx` (`id_no`);
 
 --
 -- Indexes for table `health_personnels_notification`
 --
-ALTER TABLE `health_personnels_notification`
+ALTER TABLE mylpuhims_db.`health_personnels_notification`
   ADD PRIMARY KEY (`notification_id`),
   ADD KEY `fk_notifications_copy1_health_personnels1_idx` (`id_no`);
 
 --
 -- Indexes for table `health_records`
 --
-ALTER TABLE `health_records`
+ALTER TABLE mylpuhims_db.`health_records`
   ADD PRIMARY KEY (`record_id`,`id_no`),
   ADD UNIQUE KEY `file_path_UNIQUE` (`file_path`),
   ADD KEY `fk_health_record_lyceans1_idx` (`id_no`);
@@ -312,34 +312,34 @@ ALTER TABLE `health_records`
 --
 -- Indexes for table `lyceans`
 --
-ALTER TABLE `lyceans`
+ALTER TABLE mylpuhims_db.`lyceans`
   ADD PRIMARY KEY (`id_no`);
 
 --
 -- Indexes for table `lyceans_account`
 --
-ALTER TABLE `lyceans_account`
+ALTER TABLE mylpuhims_db.`lyceans_account`
   ADD PRIMARY KEY (`id_no`),
   ADD KEY `fk_lycean_credentials_lyceans1_idx` (`id_no`);
 
 --
 -- Indexes for table `lyceans_notification`
 --
-ALTER TABLE `lyceans_notification`
+ALTER TABLE mylpuhims_db.`lyceans_notification`
   ADD PRIMARY KEY (`notification_id`),
   ADD KEY `fk_notifications_copy1_lyceans1_idx` (`id_no`);
 
 --
 -- Indexes for table `medical_files`
 --
-ALTER TABLE `medical_files`
+ALTER TABLE mylpuhims_db.`medical_files`
   ADD PRIMARY KEY (`file_id`),
   ADD KEY `fk_medical_files_consultations1_idx` (`consultation_no`);
 
 --
 -- Indexes for table `medicines`
 --
-ALTER TABLE `medicines`
+ALTER TABLE mylpuhims_db.`medicines`
   ADD PRIMARY KEY (`product_id`);
 
 --
@@ -349,31 +349,31 @@ ALTER TABLE `medicines`
 --
 -- AUTO_INCREMENT for table `activity_logs`
 --
-ALTER TABLE `activity_logs`
+ALTER TABLE mylpuhims_db.`activity_logs`
   MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `health_personnels_notification`
 --
-ALTER TABLE `health_personnels_notification`
+ALTER TABLE mylpuhims_db.`health_personnels_notification`
   MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `health_records`
 --
-ALTER TABLE `health_records`
+ALTER TABLE mylpuhims_db.`health_records`
   MODIFY `record_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `lyceans_notification`
 --
-ALTER TABLE `lyceans_notification`
+ALTER TABLE mylpuhims_db.`lyceans_notification`
   MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `medical_files`
 --
-ALTER TABLE `medical_files`
+ALTER TABLE mylpuhims_db.`medical_files`
   MODIFY `file_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -383,50 +383,50 @@ ALTER TABLE `medical_files`
 --
 -- Constraints for table `batches`
 --
-ALTER TABLE `batches`
+ALTER TABLE mylpuhims_db.`batches`
   ADD CONSTRAINT `fk_batches_medicines1` FOREIGN KEY (`product_id`) REFERENCES `medicines` (`product_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `consultations`
 --
-ALTER TABLE `consultations`
+ALTER TABLE mylpuhims_db.`consultations`
   ADD CONSTRAINT `fk_consultations_clinic_personnels1` FOREIGN KEY (`personnel_id_no`) REFERENCES `health_personnels` (`id_no`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_consultations_lyceans1` FOREIGN KEY (`lycean_id_no`) REFERENCES `lyceans` (`id_no`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `health_personnels_account`
 --
-ALTER TABLE `health_personnels_account`
+ALTER TABLE mylpuhims_db.`health_personnels_account`
   ADD CONSTRAINT `fk_clinic_personnels_credentials_clinic_personnels` FOREIGN KEY (`id_no`) REFERENCES `health_personnels` (`id_no`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `health_personnels_notification`
 --
-ALTER TABLE `health_personnels_notification`
+ALTER TABLE mylpuhims_db.`health_personnels_notification`
   ADD CONSTRAINT `fk_notifications_copy1_health_personnels1` FOREIGN KEY (`id_no`) REFERENCES `health_personnels` (`id_no`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `health_records`
 --
-ALTER TABLE `health_records`
+ALTER TABLE mylpuhims_db.`health_records`
   ADD CONSTRAINT `fk_health_record_lyceans1` FOREIGN KEY (`id_no`) REFERENCES `lyceans` (`id_no`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `lyceans_account`
 --
-ALTER TABLE `lyceans_account`
+ALTER TABLE mylpuhims_db.`lyceans_account`
   ADD CONSTRAINT `fk_lycean_credentials_lyceans1` FOREIGN KEY (`id_no`) REFERENCES `lyceans` (`id_no`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `lyceans_notification`
 --
-ALTER TABLE `lyceans_notification`
+ALTER TABLE mylpuhims_db.`lyceans_notification`
   ADD CONSTRAINT `fk_notifications_copy1_lyceans1` FOREIGN KEY (`id_no`) REFERENCES `lyceans` (`id_no`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `medical_files`
 --
-ALTER TABLE `medical_files`
+ALTER TABLE mylpuhims_db.`medical_files`
   ADD CONSTRAINT `fk_medical_files_consultations1` FOREIGN KEY (`consultation_no`) REFERENCES `consultations` (`consultation_no`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
@@ -435,4 +435,4 @@ COMMIT;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 
-INSERT INTO `administrators` (`admin_id`, `username`, `password`, `admin_name`, `locked`) VALUES (202100001, 'admin', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'Administrator', 0);
+INSERT INTO mylpuhims_db.`administrators` (`admin_id`, `username`, `password`, `admin_name`, `locked`) VALUES (202100001, 'admin', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'Administrator', 0);
